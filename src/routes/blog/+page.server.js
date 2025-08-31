5c8bd57f27dcbfd87cc4765d47804fa7093c4089
sonsodockje <hanaroo0115@naver.com>
2025-08-31 19:28:51 +0900

    블로그 페이지 리다이렉트 경로를 동적으로 수정

--- src/routes/blog/+page.server.js
+++ src/routes/blog/+page.server.js
@@ -1,6 +1,7 @@
 import { redirect } from '@sveltejs/kit';
+import { base } from '$app/paths';
 
 /** @type {import('./$types').PageServerLoad} */
 export function load() {
-	throw redirect(308, '/blog/page/1');
+	throw redirect(308, `${base}/blog/page/1`);
 }
